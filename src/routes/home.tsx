869b4dd54d0815a2583b6f0968e44ae365e80328
EimantasBlazevicius <eima.blaz@gmail.com>
2022-12-09 16:31:01 +0200

    added scrilling button

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 import HeroText from "../components/heroText.component";
-import { Grid } from "@mui/material";
+import { Grid, IconButton, Stack } from "@mui/material";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 
 export default function Home() {
   const styles = {
     heroImage: {
-      height: "95vh",
+      height: "80vh",
     },
     partTwo: {
       height: "95vh",
     },
   };
 
+  const ref = React.useRef<null | HTMLDivElement>(null);
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <Grid container>
       <Grid item xs={12} style={styles.heroImage} bgcolor="#f5daa3">
         <HeroText />
+        <Stack justifyContent="center" mt="10vh">
+          <IconButton
+            style={{ margin: "auto" }}
+            aria-label="show more"
+            onClick={handleClick}
+          >
+            <KeyboardDoubleArrowDownIcon />
+          </IconButton>
+        </Stack>
       </Grid>
-      <Grid item xs={12} style={styles.partTwo} bgcolor="text.disabled"></Grid>
+      <Grid
+        item
+        xs={12}
+        ref={ref}
+        style={styles.partTwo}
+        bgcolor="text.disabled"
+      ></Grid>
+      <Grid item xs={12} style={styles.partTwo} bgcolor="primary.main"></Grid>
     </Grid>
   );
 }
